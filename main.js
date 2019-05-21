61fc88563f04eb3068b02155211cc9fa48d9c329
TERRIER Bastien <bastien.terrier@gmail.com>
2019-05-21 15:55:29 +0200

    test

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world!!!');
+const fs = require('fs-extra');
+const jsyaml = require('js-yaml');
+
+const file = fs.readFileSync('assets/json.rulesrc');
+try {
+  const parsed = JSON.parse(file);
+  console.log('JSON!');
+  console.log(parsed.rules[0].onSuccess);
+} catch (e) {
+  const parsed = jsyaml.safeLoad(file);
+  console.log('YAML!');
+  console.log(parsed.rules[0].onSuccess);
+}
