64f441b5e29c98cc85153068ca66bbcf0d877b2e
TERRIER Bastien <bastien.terrier@gmail.com>
2019-05-21 16:01:10 +0200

    test '

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const jsyaml = require('js-yaml');
 const file = fs.readFileSync('assets/json.rulesrc');
 try {
   const parsed = JSON.parse(file);
-  console.log('JSON');
+  console.log('JSON!');
   console.log(parsed.rules[0].onSuccess);
 } catch (e) {
   const parsed = jsyaml.safeLoad(file);
